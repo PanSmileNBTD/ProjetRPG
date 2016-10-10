6dedd9beb9843b473354ebb8772d0483aa9ca092
PanSmileNBTD <wellandmottet@gmail.com>
2016-10-10 09:06:43 +0200

    Ajout de collisions

--- SimpleSlickGame.java
+++ SimpleSlickGame.java
@@ -17,17 +17,19 @@ public class SimpleSlickGame extends BasicGame {
     // Carte
     private TiledMap map;
 
-    private float x = 300, y = 300;
+    private float x = 500, y = 500;
     private float xCamera = x, yCamera = y;
     private int direction = 0;
     private int oldDirection = -1;
     private boolean moving = false;
     private Animation[] animations = new Animation[8];
 
+    private String texte = "";
+
     @Override
     public void init(GameContainer gc) throws SlickException {
         this.container = gc;
-        this.map = new TiledMap("resources/test.tmx");
+        this.map = new TiledMap("resources/carte.tmx");
 
         SpriteSheet spriteSheet = new SpriteSheet(("resources/perso/character.png"), 64, 64);
 
@@ -143,21 +145,37 @@ public class SimpleSlickGame extends BasicGame {
 
     @Override
     public void update(GameContainer gc, int i) throws SlickException {
+        // Trigger
+        // ...
+
         // Si le personnage est censé bouger, on le déplace
         if(this.moving) {
-            switch(this.direction) {
-                case 0:
-                    this.y -= .1f * i;
-                    break;
-                case 1:
-                    this.x -= .1f * i;
-                    break;
-                case 2:
-                    this.y += .1f * i;
-                    break;
-                case 3:
-                    this.x += .1f * i;
-                    break;
+            if(this.moving) {
+                float futurX = this.x;
+                float futurY = this.y;
+                switch(this.direction) {
+                    case 0:
+                        futurY = this.y - .2f * i;
+                        break;
+                    case 1:
+                        futurX = this.x - .2f * i;
+                        break;
+                    case 2:
+                        futurY = this.y + .2f * i;
+                        break;
+                    case 3:
+                        futurX = this.x + .2f * i;
+                        break;
+                }
+
+                boolean collision = isCollision(futurX, futurY);
+
+                if(collision) {
+                    this.moving = false;
+                } else {
+                    this.x = futurX;
+                    this.y = futurY;
+                }
             }
         }
 
@@ -178,18 +196,38 @@ public class SimpleSlickGame extends BasicGame {
         }
     }
 
+    private boolean isCollision(float x, float y) {
+        int tileW = this.map.getTileWidth();
+        int tileH = this.map.getTileHeight();
+        int solidLayer = this.map.getLayerIndex("solid");
+        Image tile = this.map.getTileImage((int)x / tileW, (int)y / tileH, solidLayer);
+        boolean collision = tile != null;
+        if(collision) {
+            Color color = tile.getColor((int)x % tileW, (int)y % tileH);
+            collision = color.getAlpha() > 0;
+        }
+        return collision;
+    }
+
     @Override
     public void render(GameContainer gc, Graphics g) {
 
         g.translate(gc.getWidth() / 2 - (int) this.xCamera, gc.getHeight() / 2 - (int) this.yCamera);
 
-        this.map.render(0, 0);
-        g.drawString("Hello World!", 100, 100);
+        // Premiers calques
+        this.map.render(0, 0, 0);
+        this.map.render(0, 0, 1);
+        this.map.render(0, 0, 2);
 
         // Personnage :
         g.setColor(new Color(0, 0, 0, .5f));
         g.fillOval(x - 16, y - 8, 32, 16);
         g.drawAnimation(animations[direction + (moving ? 4 : 0)], x - 32, y - 60);
+
+        // Derniers calques
+        this.map.render(0, 0, 3);
+
+
     }
 
     public static void main(String[] args) {
